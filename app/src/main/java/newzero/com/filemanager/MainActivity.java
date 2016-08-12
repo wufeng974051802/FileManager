51ea400bb4cdf9737888c0ff9a33f6a6c2180eaf
unknown <wufeng974051802@gmail.com>
2016-08-12 17:44:15 +0800

    Revert "modify by wufeng rm the MainAcitivity"
    This reverts commit ea586e8c230ae67f04af52e2ed74b9f499ab3d7e.

---
+++ app/src/main/java/newzero/com/filemanager/MainActivity.java
@@ -0,0 +1,19 @@
+package newzero.com.filemanager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+
+public class MainActivity extends BaseDrawerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
